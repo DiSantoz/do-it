a1bddca9306f10b2827b4f451e7b97122565e11b
David Ferreira <dsferreira06@gmail.com>
2022-04-21 18:21:39 -0400

    add lower/uppcase use case

--- app.js
+++ app.js
@@ -38,16 +38,16 @@ list.addEventListener("click", (e) => {
 // filter todos
 const filterTodos = (term) => {
   Array.from(list.children)
-    .filter((todo) => !todo.textContent.includes(term))
+    .filter((todo) => !todo.textContent.toLowerCase().includes(term))
     .forEach((todo) => todo.classList.add("filtered"));
 
   Array.from(list.children)
-    .filter((todo) => todo.textContent.includes(term))
+    .filter((todo) => todo.textContent.toLowerCase().includes(term))
     .forEach((todo) => todo.classList.remove("filtered"));
 };
 
 //keyup event
 search.addEventListener("keyup", () => {
-  const term = search.value.trim();
+  const term = search.value.trim().toLowerCase();
   filterTodos(term);
 });
